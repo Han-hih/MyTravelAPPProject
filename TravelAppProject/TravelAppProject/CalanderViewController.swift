808b11e54c75ad6be21448580bd019babe8038be
한 <hih458@naver.com>
2023-09-26 16:54:39 +0900

    add 나라 선택 후 날짜 선택을 위한 뷰컨트롤러 생성

---
+++ TravelAppProject/TravelAppProject/CalanderViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CalanderViewController.swift
+//  TravelAppProject
+//
+//  Created by 황인호 on 2023/09/26.
+//
+
+import UIKit
+
+class CalanderViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green 
+    }
+}
