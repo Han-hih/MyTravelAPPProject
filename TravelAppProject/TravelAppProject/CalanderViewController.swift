52c2d10050329a53c15708dc1fc8f88ecddc1c1e
한 <hih458@naver.com>
2023-09-27 16:44:49 +0900

    feat 달력에서 다중선택이 가능하도록 구현
    - 기간으로 날짜 선택

--- TravelAppProject/TravelAppProject/CalanderViewController.swift
+++ TravelAppProject/TravelAppProject/CalanderViewController.swift
@@ -6,13 +6,105 @@
 //
 
 import UIKit
+import FSCalendar
 
-class CalanderViewController: UIViewController {
+class CalanderViewController: UIViewController, FSCalendarDelegate {
     
+    lazy var calendar = {
+        let view = FSCalendar()
+        view.delegate = self
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
+    private var datesRange: [Date]?
+    private var firstDate: Date?
+    private var lastDate: Date?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green 
+        view.backgroundColor = .white
+        view.addSubview(calendar)
+        setAutoLayout()
+        calendar.allowsMultipleSelection = true
+    }
+    
+    func setAutoLayout() {
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            calendar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            calendar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            calendar.heightAnchor.constraint(equalToConstant: 400)
+        ])
+    }
+    
+    func datesRange(from: Date, to: Date) -> [Date] {
+        if from > to { return [Date]() }
+        
+        var tempDate = from
+        var array = [tempDate]
+        
+        while tempDate < to {
+            tempDate = Calendar.current.date(byAdding: .day, value: 1, to: tempDate)!
+            array.append(tempDate)
+        }
+        
+        return array
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        // nothing selected:
+        if firstDate == nil {
+            firstDate = date
+            datesRange = [firstDate!]
+            
+            print("datesRange contains: \(datesRange!)")
+            return
+        }
+        if firstDate != nil && lastDate == nil {
+            if date <= firstDate! {
+                calendar.deselect(firstDate!)
+                firstDate = date
+                datesRange = [firstDate!]
+                
+                print("datesRange contains: \(datesRange!)")
+                return
+            }
+            
+            let range = datesRange(from: firstDate!, to: date)
+            lastDate = range.last
+            
+            for d in range {
+                calendar.select(d)
+            }
+            datesRange = range
+            print("datesRange contains: \(datesRange!)")
+            return
+        }
+        
+        if firstDate != nil && lastDate != nil {
+            for d in calendar.selectedDates {
+                calendar.deselect(d)
+            }
+            
+            lastDate = nil
+            firstDate = nil
+            
+            datesRange = []
+            
+            print("datesRange contains: \(datesRange!)")
+        }
+    }
+    
+    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        if firstDate != nil && lastDate != nil {
+            for d in calendar.selectedDates {
+                calendar.deselect(d)
+            }
+            lastDate = nil
+            firstDate = nil
+            datesRange = []
+            print("datesRange contains: \(datesRange!)")
+        }
     }
 }
