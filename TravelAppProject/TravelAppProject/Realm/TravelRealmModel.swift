fc94a834510b643aea58aaa6d7840091c0412f7c
한 <hih458@naver.com>
2023-10-05 22:29:16 +0900

    feat realm모델 생성

---
+++ TravelAppProject/TravelAppProject/Realm/TravelRealmModel.swift
@@ -0,0 +1,55 @@
+//
+//  RealmModel.swift
+//  TravelAppProject
+//
+//  Created by 황인호 on 2023/10/05.
+//
+
+import Foundation
+import RealmSwift
+
+class TravelRealmModel: Object {
+    
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var country: String
+    @Persisted var startDate: Date
+    @Persisted var endDate: Date?
+    @Persisted var detail: List<DetailTable>
+    
+    convenience init(country: String, startDate: Date, endDate: Date? = nil, detail: List<DetailTable>) {
+        self.init()
+        
+        self.country = country
+        self.startDate = startDate
+        self.endDate = endDate
+        self.detail = detail
+    }
+    
+    
+}
+
+class DetailTable: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var date: Date
+    @Persisted var sequence: Int
+    @Persisted var location: String
+    @Persisted var memo: String?
+    @Persisted var time: String?
+    @Persisted var longitude: Double
+    @Persisted var latitude: Double
+    
+    @Persisted(originProperty: "detail") var mainPlan: LinkingObjects<TravelRealmModel>
+    
+    convenience init(date: Date, sequence: Int, location: String, memo: String? = nil, time: String? = nil, longitude: Double, latitude: Double, mainPlan: LinkingObjects<TravelRealmModel>) {
+        self.init()
+        
+        self.date = date
+        self.sequence = sequence
+        self.location = location
+        self.memo = memo
+        self.time = time
+        self.longitude = longitude
+        self.latitude = latitude
+        self.mainPlan = mainPlan
+    }
+}
