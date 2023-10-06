348ff30ab613267dad8035a12d95990c2c5166d0
한 <hih458@naver.com>
2023-10-06 21:19:21 +0900

    add reusableProtocol 생성

---
+++ TravelAppProject/TravelAppProject/ReusableViewProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  ReusableViewProtocol.swift
+//  TravelAppProject
+//
+//  Created by 황인호 on 2023/10/06.
+//
+
+import UIKit
+
+protocol ReusableViewProtocol {
+    static var identifier: String { get }
+}
+
+extension UICollectionViewCell: ReusableViewProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
