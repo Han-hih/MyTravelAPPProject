de72305718991baf6ac84ecdd755fba3710aec4f
한 <hih458@naver.com>
2023-10-12 14:54:40 +0900

    feat 기기에 사진저장을 위한 FileManager 생성

---
+++ TravelAppProject/TravelAppProject/Utility/FileManager+Extension.swift
@@ -0,0 +1,52 @@
+//
+//  FileManager+Extension.swift
+//  TravelAppProject
+//
+//  Created by 황인호 on 2023/10/12.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func documentDirectoryPath() -> URL? {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        return documentDirectory
+    }
+    
+    func removeImageFromDocument(fileName: String) {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        let fileURL = documentDirectory.appendingPathComponent(fileName)
+        
+        do {
+            try FileManager.default.removeItem(at: fileURL)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func loadImageFromDocument(fileName: String) -> UIImage {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return UIImage(systemName: "star.fill")! }
+        let fileURL = documentDirectory.appendingPathComponent(fileName)
+        if FileManager.default.fileExists(atPath: fileURL.path) {
+            return UIImage(contentsOfFile: fileURL.path)!
+        } else {
+            return UIImage(systemName: "star.fill")!
+        }
+    }
+    
+    func saveImageToDocument(fileName: String, image: UIImage) {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        let fileURL = documentDirectory.appendingPathComponent(fileName)
+        print(fileURL)
+        //file:///Users/inhohwang/Library/Developer/CoreSimulator/Devices/85F189C8-FB75-4D62-BFA0-656B451AA773/data/Containers/Data/Application/C36EB616-80F0-4ACB-A5C4-34F923B4AF98/Documents/ + fileName
+        guard let data = image.jpegData(compressionQuality: 0.5) else { return }
+        
+        do {
+            try data.write(to: fileURL)
+        } catch let error {
+            print("file save error", error)
+        }
+    }
+}
