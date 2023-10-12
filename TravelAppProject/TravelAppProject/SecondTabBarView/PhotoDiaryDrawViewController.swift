ed6ff5c21be33bda6dca742192aa4779099f976d
한 <hih458@naver.com>
2023-10-13 01:12:07 +0900

    add 오토레이아웃 설정 및 버튼에 동작 연결

--- TravelAppProject/TravelAppProject/SecondTabBarView/PhotoDiaryDrawViewController.swift
+++ TravelAppProject/TravelAppProject/SecondTabBarView/PhotoDiaryDrawViewController.swift
@@ -8,34 +8,73 @@
 import UIKit
 import PhotosUI
 
-class PhotoDiaryDrawViewController: UIViewController, PHPickerViewControllerDelegate {
+final class PhotoDiaryDrawViewController: UIViewController, PHPickerViewControllerDelegate {
     
    private let photoButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "photo.badge.plus"), for: .normal)
         button.addTarget(self, action: #selector(photoButtonTapped), for: .touchUpInside)
-       
+       button.setTitleColor(.black, for: .normal)
        return button
     }()
     
     private let photoView = {
         let view = UIImageView()
         view.backgroundColor = .clear
-       
+        view.isUserInteractionEnabled = true
+//        view.layer.cornerRadius = 20
+//        view.clipsToBounds = true
         return view
     }()
     
-    private let memoView = {
+    private let memoTextFieldView = {
         let view = UITextView()
         view.backgroundColor = .lightGray
+        let keyboardToolbar = UIToolbar()
+        let flexBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneBarButton = UIBarButtonItem(title: "Done".localized, style: .plain, target: self, action: #selector(doneButtonTapped))
+        keyboardToolbar.items = [flexBarButton, doneBarButton]
+        keyboardToolbar.sizeToFit()
+        keyboardToolbar.tintColor = UIColor.systemGray
+        view.inputAccessoryView = keyboardToolbar
         return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        setLayout()
+        
     }
- 
+    
+    @objc func doneButtonTapped() {
+        self.view.endEditing(true)
+    }
+    
+    func setLayout() {
+        photoView.addSubview(photoButton)
+        photoButton.translatesAutoresizingMaskIntoConstraints = false
+        [photoView, memoTextFieldView].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        NSLayoutConstraint.activate([
+            photoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            photoView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            photoView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            photoView.heightAnchor.constraint(equalTo: photoView.widthAnchor, multiplier: 1),
+            
+            photoButton.trailingAnchor.constraint(equalTo: photoView.trailingAnchor),
+            photoButton.bottomAnchor.constraint(equalTo: photoView.bottomAnchor),
+            photoButton.heightAnchor.constraint(equalToConstant: 50),
+            photoButton.widthAnchor.constraint(equalTo: photoButton.heightAnchor),
+            
+            memoTextFieldView.topAnchor.constraint(equalTo: photoView.bottomAnchor, constant: 30),
+            memoTextFieldView.leadingAnchor.constraint(equalTo: photoView.leadingAnchor),
+            memoTextFieldView.trailingAnchor.constraint(equalTo: photoView.trailingAnchor),
+            memoTextFieldView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.3)
+        ])
+    }
     
     @objc func photoButtonTapped() {
         var configuration = PHPickerConfiguration()
@@ -43,6 +82,7 @@ class PhotoDiaryDrawViewController: UIViewController, PHPickerViewControllerDele
         
         let picker = PHPickerViewController(configuration: configuration)
         picker.delegate = self
+        
         self.present(picker, animated: true)
     }
     
@@ -56,6 +96,7 @@ class PhotoDiaryDrawViewController: UIViewController, PHPickerViewControllerDele
             itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
                 DispatchQueue.main.async {
                     self.photoView.image = image as? UIImage
+                    self.photoView.contentMode = .scaleAspectFit
                 }
             }
         } else {
