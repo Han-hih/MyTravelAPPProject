799d691b641b9112edf4f3c21f67476d5757c649
한 <hih458@naver.com>
2023-10-13 10:27:40 +0900

    add PhotoRealmRepository 생성

---
+++ TravelAppProject/TravelAppProject/Realm/PhotoRealmRepository.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoRealmRepository.swift
+//  TravelAppProject
+//
+//  Created by 황인호 on 2023/10/13.
+//
+
+import Foundation
+import RealmSwift
+
+protocol PhotoRealmRepositoryType: AnyObject {
+    func createItem(_ item: PhotoTable)
+}
+
+class PhotoRealmRepository: PhotoRealmRepositoryType {
+    
+   private let realm = try! Realm()
+    
+    func createItem(_ item: PhotoTable) {
+        
+        do {
+            try realm.write {
+                realm.add(item)
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
