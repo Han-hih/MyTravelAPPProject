0574520a6b692c5522d38922110c9915b4a1d8f5
한 <hih458@naver.com>
2023-10-20 17:02:01 +0900

    fix 셀을 삭제했을 때 index 문제 해결

--- TravelAppProject/TravelAppProject/PlanViewController.swift
+++ TravelAppProject/TravelAppProject/PlanViewController.swift
@@ -8,9 +8,7 @@
 import UIKit
 import RealmSwift
 
-final class PlanViewController: UIViewController {
-    
-    
+final class PlanViewController: UIViewController, UIPopoverPresentationControllerDelegate {
     
     var id: ObjectId?
     
@@ -20,7 +18,7 @@ final class PlanViewController: UIViewController {
     
     var appendArr = [String]()
     var sectionCount = 0
-    lazy var place = [[Plan]](repeating: [Plan(objectID: id!, location: "", memo: "", time: "")], count: sectionCount)
+    lazy var place = [[Plan]](repeating: [], count: sectionCount)
     var dateArray = [Date]()
     
     let tableView = {
@@ -67,7 +65,6 @@ final class PlanViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         list = realm.objects(TravelRealmModel.self)
-        
         view.backgroundColor = .white
         setNavigation()
         setAutoLayout()
@@ -79,18 +76,32 @@ final class PlanViewController: UIViewController {
     func setSortedData() {
         // 날짜 기준으로 하나의 Array 만들어서 sortedData 추가
         let main = realm.objects(TravelRealmModel.self).where {
-                    $0._id == id!
-                }.first!
-        
+            $0._id == id!
+        }.first!
+        place = [[Plan]](repeating: [], count: sectionCount)
         for i in 0..<sectionCount {
             for j in 0..<main.detail.count {
                 if dateArray[i] == main.detail[j].date {
                     place[i].append(Plan(objectID: main.detail[j]._id, location: main.detail[j].location))
                 }
             }
-            if place[i][0].location == "" {
-                           place[i].remove(at: 0)
-                       }
+            
+        }
+    }
+    
+    func setData() {
+        let main = realm.objects(TravelRealmModel.self).where {
+            $0._id == id!
+        }.first!
+        place = [[Plan]](repeating: [], count: sectionCount)
+        print(sectionCount, main.detail.count)
+        for i in 0..<sectionCount {
+            for j in 0..<main.detail.count {
+                if dateArray[i] == main.detail[j].date {
+                    place[i].append(Plan(objectID: main.detail[j]._id, location: main.detail[j].location))
+                }
+            }
+
         }
     }
 
@@ -163,24 +174,32 @@ extension PlanViewController: UITableViewDelegate, UITableViewDataSource {
         let main = self.realm.objects(TravelRealmModel.self).where {
             $0._id == self.id!
         }.first!
+        print(main)
         let deleteAction = UIContextualAction(style: .destructive, title: nil) { _, _, completionHandler in
+                print(main.detail.count)
                 for i in 0..<main.detail.count {
                     guard let task = self.realm.objects(DetailTable.self).filter({ $0._id == main.detail[i]._id }).first else { return }
                     if self.place[indexPath.section][indexPath.row].objectId == main.detail[i]._id {
                         try! self.realm.write {
                             self.realm.delete(task)
                         }
-                        
+
                     }
                 }
+
+            // 테이블 뷰에서 해당 셀 삭제
+            self.setData()
+//            tableView.deleteRows(at: [indexPathToDelete], with: .automatic)
             
-                self.tableView.reloadData()
-            
-            
-            
+            print(self.place)
+            tableView.reloadData()
+
+
             print("삭제 클릭됨")
             completionHandler(true)
+            
         }
+        
         deleteAction.image = UIImage(systemName: "trash")
         deleteAction.backgroundColor = .red
         let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
@@ -203,8 +222,12 @@ extension PlanViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: PlanTableViewCell.identifier, for: indexPath) as? PlanTableViewCell else {
             return UITableViewCell() }
+        cell.selectionStyle = .none
+        
         
         cell.placeLabel.text = place[indexPath.section][indexPath.row].location
+        
+        cell.memoButton.addTarget(self, action: #selector(showPopup), for: .touchUpInside)
         return cell
         
     }
